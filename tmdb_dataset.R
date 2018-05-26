a29ae2e0032bc90040197586400b734be35cf39c
katiechen00 <chenzi7@uw.edu>
2018-05-25 23:47:14 -0700

    update new one

--- tmdb_dataset.R
+++ tmdb_dataset.R
@@ -25,7 +25,3 @@ make_csv <- function(pagenum) {
   return(last_page)
 }
 
-response <- GET("https://api.themoviedb.org/3/discover/movie?api_key=d0c718e2a61b92b6173965005833dea8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2")
-response_content <- content(response, type = "text")
-body <- fromJSON(response_content)
-

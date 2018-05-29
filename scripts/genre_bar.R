e0145846658e9d44d66e230fc15f8b79abc569a0
Leona Wada <wadal@uw.edu>
2018-05-29 13:17:42 -0700

    bar

--- scripts/genre_bar.R
+++ scripts/genre_bar.R
@@ -17,12 +17,27 @@ response <- GET(paste0("https://api.themoviedb.org/3/genre/movie/list?api_key=",
 response_content <- content(response, type = "text")
 genre_list <- fromJSON(response_content)$genres
 
+## Make dataframe for each genre
 data <- select(data, title, release_year, genre_ids) 
 action <- data[grepl("28", data$genre_ids),]
 adventure <- data[grepl("12", data$genre_ids),]
 animation <- data[grepl("16", data$genre_ids),]
 comedy <-data[grepl("35", data$genre_ids),]
-
+crime <-data[grepl("80", data$genre_ids),]
+documentary <-data[grepl("99", data$genre_ids),]
+drama <-data[grepl("18", data$genre_ids),]
+family <-data[grepl("10751", data$genre_ids),]
+fantasy <-data[grepl("14", data$genre_ids),]
+history <-data[grepl("36", data$genre_ids),]
+horror <-data[grepl("27", data$genre_ids),]
+music <-data[grepl("10402", data$genre_ids),]
+mystery <-data[grepl("9648", data$genre_ids),]
+romance <-data[grepl("10749", data$genre_ids),]
+sci_fi <-data[grepl("878", data$genre_ids),]
+tv <-data[grepl("10770", data$genre_ids),]
+thriller <-data[grepl("53", data$genre_ids),]
+war <-data[grepl("10752", data$genre_ids),]
+western <-data[grepl("37", data$genre_ids),]
 
 
 build_bar <- function(data, yearvar) {
@@ -32,6 +47,38 @@ build_bar <- function(data, yearvar) {
   adventure_count <- nrow(adventure)
   animation <- filter(animation, release_year == yearvar)
   animation_count <- nrow(animation)
+  comedy <- filter(comedy, release_year == yearvar)
+  comedy_count <- nrow(comedy)
+  crime <- filter(crime, release_year == yearvar)
+  crime_count <- nrow(crime)
+  documentary <- filter(documentary, release_year == yearvar)
+  doc_count <- nrow(documentary)
+  drama <- filter(drama, release_year == drama)
+  drama_count <- nrow(drama)
+  family <- filter(family, release_year == yearvar)
+  fam_count <- nrow(family)
+  fantasy <- filter(fantasy, release_year == yearvar)
+  fantasy_count <- nrow(fantasy)
+  history <- filter(history, release_year == yearvar)
+  hist_count <- nrow(history)
+  horror <- filter(horror, release_year == yearvar)
+  horror_count <- nrow(horror)
+  music <- filter(music, release_year == yearvar)
+  music_count <- nrow(music)
+  mystery <- filter(mystery, release_year == yearvar)
+  mystery_count <- nrow(mystery)
+  romance <- filter(romance, release_year == yearvar)
+  rom_count <- nrow(romance)
+  sci_fi <- filter(sci_fi, release_year == yearvar)
+  sci_fi_count <- nrow(sci_fi)
+  tv <- filter(tv, release_year == yearvar)
+  tv_count <- nrow(tv)
+  thriller <- filter(thriller, release_year == yearvar)
+  thriller_count <- row(thriller)
+  war <- filter(war, release_year == yearvar)
+  war_count <- nrow(war)
+  western <- filter(western, release_year == yearvar)
+  western_count <- nrow(western)
     
   # a function to create a bar chart
   plot_ly(data,
@@ -40,26 +87,25 @@ build_bar <- function(data, yearvar) {
   ) %>%
     add_trace(y = ~ adventure_count, name = "Adventure") %>%
     add_trace(y = ~ animation_count, name = "Animation") %>%
+    add_trace(y = ~ comedy_count, name = "Comedy") %>%
+    add_trace(y = ~ crime_count, name = "Crime") %>%
+    add_trace(y = ~ doc_count, name = "Documentary") %>%
+    add_trace(y = ~ drama_count, name = "Drama") %>%
+    add_trace(y = ~ fam_count, name = "Family") %>%
+    add_trace(y = ~ fantasy_count, name = "Fantasy") %>%
+    add_trace(y = ~ hist_count, name = "History") %>%
+    add_trace(y = ~ horror_count, name = "Horror") %>%
+    add_trace(y = ~ music_count, name = "Music") %>%
+    add_trace(y = ~ mystery_count, name = "Mystery") %>%
+    add_trace(y = ~ romance_count, name = "Romance") %>%
+    add_trace(y = ~ sci_fi_count, name = "Sci-Fi") %>%
+    add_trace(y = ~ tv_count, name = "TV") %>%
+    add_trace(y = ~ thriller_count, name = "Thriller") %>%
+    add_trace(y = ~ western_count, name = "Western") %>%
+    add_trace(y = ~ war_count, name = "War") %>%
     layout(
       yaxis = list(title = "count", xaxis = list(title = "Release Year"),
                    barmode = "Group")
     )
 }
 
-
-build_graph2 <- function(df, statevar2) {
-  race_df <- filter(df, state == statevar2)
-  # a function to create a bar chart
-  plot_ly(race_df,
-          x = ~ statevar2, y = ~ White, type = "bar",
-          name = "White"
-  ) %>%
-    add_trace(y = ~ Black, name = "Black") %>%
-    add_trace(y = ~ Native_American, name = "Native American") %>%
-    add_trace(y = ~ Asian, name = "Asian") %>%
-    add_trace(y = ~ Other, name = "Other") %>%
-    layout(
-      yaxis = list(title = "People"), xaxis = list(title = "State"),
-      barmode = "Group"
-    )
-}
